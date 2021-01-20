4c1051a6a72fe4d16ca92d2aa085eed983c9a139
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-20 11:54:04 +0530

    deploy: setup gql api + cognito user pool

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -4,4 +4,4 @@ import * as cdk from '@aws-cdk/core';
 import { DeployStack } from '../lib/deploy-stack';
 
 const app = new cdk.App();
-new DeployStack(app, 'DeployStack');
+new DeployStack(app, 'openh-chat-stack-ash');
--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,9 +1,54 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { AuthorizationType, GraphqlApi, Schema } from "@aws-cdk/aws-appsync";
+import { UserPool } from "@aws-cdk/aws-cognito";
+import { CfnOutput } from "@aws-cdk/core";
+import { AttributeType, BillingMode, Table } from "@aws-cdk/aws-dynamodb";
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const userPool = new UserPool(this, "AwsChatUserPool", {
+      autoVerify: {
+        email: true,
+      },
+      userPoolName: "openh-chat-user-pool",
+    });
+
+    const api = new GraphqlApi(this, "AwsChatApi", {
+      name: "openh-chat-gql-api",
+      schema: Schema.fromAsset("../graphql/schema.graphql"),
+      xrayEnabled: true,
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.USER_POOL,
+          userPoolConfig: {
+            userPool,
+          },
+        },
+      },
+    });
+
+    new CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    });
+
+    new CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
+
+    new CfnOutput(this, "Stack Region", {
+      value: this.region,
+    });
+
+    const usersTable = new Table(this, "AwsChatUsersTable", {
+      tableName: "openh-chat-users-table",
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+    });
   }
 }
