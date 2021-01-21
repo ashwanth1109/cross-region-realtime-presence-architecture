153aace86bc931b5b4e96403058b0371f7a89c31
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-21 07:08:51 +0530

    feature: wss communication between two clients

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Auth } from "aws-amplify";
+import config from "./aws-exports";
 
 const App = () => {
-  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticated, userHasAuthenticated] = useState<null | string>(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
   async function handleSubmit(event: any) {
     event.preventDefault();
     try {
       const user = await Auth.signIn(username, password);
       console.log(user);
-      userHasAuthenticated(true);
+      userHasAuthenticated(user.username);
     } catch (e) {
       alert(e.message);
     }
   }
 
+  const constructMessage = (timestamp: number, type: string) => {
+    return JSON.stringify({
+      action: "sendmessage",
+      data: JSON.stringify({
+        type,
+        timestamp,
+        username: isAuthenticated,
+      }),
+    });
+  };
+
+  const startTyping = () => {
+    // start typing
+    const timestamp = Date.now();
+    console.log("Started typing at", timestamp);
+    socket?.send(constructMessage(timestamp, "START_TYPING"));
+  };
+
+  const stopTyping = () => {
+    // stop typing
+    const timestamp = Date.now();
+    console.log("Stopped typing at", timestamp);
+    socket?.send(constructMessage(timestamp, "STOP_TYPING"));
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // connect websocket
+      const newSocket: WebSocket = new WebSocket(config.ws_url);
+      setSocket(newSocket);
+      newSocket.onopen = (e) => {
+        console.log("Socket onopen fired", e);
+      };
+      newSocket.onmessage = (e) => {
+        console.log("Socket onmessage fired", e);
+      };
+      newSocket.onclose = (e) => {
+        console.log("Socket onclose fired", e);
+      };
+      newSocket.onerror = (e) => {
+        console.log("Socket onerror fired", e);
+      };
+    }
+  }, [isAuthenticated]);
+
   if (isAuthenticated) {
     return (
       <div>
         <h1>User is authenticated.</h1>
+        <button onClick={startTyping}>Start typing</button>
+        <button onClick={stopTyping}>Stop typing</button>
       </div>
     );
   }
