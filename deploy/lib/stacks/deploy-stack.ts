15db81c2def882b67bce1262ee725c950dff9678
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-25 03:48:27 +0530

    checkpoint: wss api setup complete

--- deploy/lib/stacks/deploy-stack.ts
+++ deploy/lib/stacks/deploy-stack.ts
@@ -9,8 +9,14 @@ import {
   Table,
 } from "@aws-cdk/aws-dynamodb";
 import { Code, Function, Runtime } from "@aws-cdk/aws-lambda";
-import { CfnApi } from "@aws-cdk/aws-apigatewayv2";
-import { withEnv } from "../util";
+import {
+  CfnApi,
+  CfnDeployment,
+  CfnIntegration,
+  CfnRoute,
+  CfnStage,
+} from "@aws-cdk/aws-apigatewayv2";
+import { constructIntegUri, withEnv } from "../util";
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -20,7 +26,7 @@ export class DeployStack extends cdk.Stack {
 
     // Cross-region replication of dynamodb tables with stream
     const tableName = withEnv("user-presence");
-    new Table(this, tableName, {
+    const table = new Table(this, tableName, {
       tableName,
       billingMode: BillingMode.PAY_PER_REQUEST,
       partitionKey: {
@@ -32,41 +38,83 @@ export class DeployStack extends cdk.Stack {
       replicationRegions: ["ap-south-1"],
     });
 
-    const connectLambda = new Function(this, withEnv("connect-function"), {
+    const environment = {
+      TABLE_NAME: table.tableName,
+    };
+
+    const functionParams = {
       runtime: Runtime.NODEJS_12_X,
-      handler: "connect/index.handler",
       code: Code.fromAsset("../dist/"),
       memorySize: 128,
+      environment,
+    };
+
+    const wssRouteNames = [
+      {
+        name: "connect",
+        key: "$connect",
+      },
+      {
+        name: "disconnect",
+        key: "$disconnect",
+      },
+      {
+        name: "message",
+        key: "message",
+      },
+    ];
+
+    const routes = wssRouteNames.map((route) => {
+      const lambdaName = withEnv(`${route.name}-function`);
+      return {
+        name: route.name,
+        key: route.key,
+        lambda: new Function(this, lambdaName, {
+          ...functionParams,
+          handler: `${route.name}/index.handler`,
+        }),
+      };
     });
 
-    const disconnectLambda = new Function(
-      this,
-      withEnv("disconnect-function"),
-      {
-        runtime: Runtime.NODEJS_12_X,
-        handler: "disconnect/index.handler",
-        code: Code.fromAsset("../dist/"),
-        memorySize: 128,
-      }
-    );
+    const wssApiName = withEnv("wss-api");
+    const wssApi = new CfnApi(this, wssApiName, {
+      name: wssApiName,
+      protocolType: "WEBSOCKET",
+      routeSelectionExpression: "$request.body.action",
+    });
 
-    const sendMessageLambda = new Function(
-      this,
-      withEnv("sendMessage-function"),
-      {
-        runtime: Runtime.NODEJS_12_X,
-        handler: "sendMessage/index.handler",
-        code: Code.fromAsset("../dist/"),
-        memorySize: 128,
-      }
-    );
+    routes.map((route) => {
+      const integName = withEnv(`${route.name}-integration`);
+      const integUri = constructIntegUri(route.lambda.functionArn, this.region);
+      const integration = new CfnIntegration(this, integName, {
+        apiId: wssApi.ref,
+        integrationType: "AWS_PROXY",
+        integrationUri: integUri,
+      });
 
-    // const wssApiName = withEnv("wss-api");
-    // const wssApi = new CfnApi(this, wssApiName, {
-    //   name: wssApiName,
-    //   protocolType: "WEBSOCKET",
-    //   routeSelectionExpression: "$request.body.action",
-    // });
+      const routeName = withEnv(`${route.name}-route`);
+      new CfnRoute(this, routeName, {
+        apiId: wssApi.ref,
+        routeKey: route.key,
+        authorizationType: "NONE",
+        target: `integrations/${integration.ref}`,
+      });
+    });
+
+    const wssDeployment = new CfnDeployment(this, withEnv("wss-deployment"), {
+      apiId: wssApi.ref,
+    });
+
+    const stageName = "test";
+    new CfnStage(this, withEnv("wss-stage"), {
+      stageName,
+      apiId: wssApi.ref,
+      deploymentId: wssDeployment.ref,
+    });
+
+    new CfnOutput(this, "WssUrl", {
+      value: `wss://${wssApi.ref}.execute-api.${this.region}.amazonaws.com/${stageName}`,
+    });
 
     // const userPool = new UserPool(this, "AwsChatUserPool", {
     //   autoVerify: {
--- deploy/lib/util.ts
+++ deploy/lib/util.ts
@@ -2,3 +2,10 @@ export const PROJECT_NAME = "openh";
 export const ENV_NAME = "ash";
 
 export const withEnv = (name: string) => `${PROJECT_NAME}-${name}-${ENV_NAME}`;
+
+export const constructIntegUri = (fnArn: string, region: string) => {
+  const arnPrefix = `arn:aws:apigateway:${region}`;
+  const lambdaPathPrefix = `lambda:path/2015-03-31/functions/`;
+  const invocation = `${lambdaPathPrefix}${fnArn}/invocations`;
+  return `${arnPrefix}:${invocation}`;
+};
