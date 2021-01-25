d619661032041ceca42025fb28decff60d4778e4
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-26 04:18:12 +0530

    Remove ignored files

--- frontend/src/aws-exports.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  region: "us-east-1",
-  user_pool_id: "us-east-1_ePmcY4dUt",
-  app_client_id: "7sgslv0vnngsqohf715ei9ci70",
-  identity_pool_id: "",
-  ws_url: "wss://1ku3elscpg.execute-api.us-east-1.amazonaws.com/test",
-};
