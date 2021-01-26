802bbba9870da503f9952e9e266b84baedf46924
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-26 17:14:27 +0530

    cli: application calculates latency and averageLatency

--- cli-client/connector.ts
+++ cli-client/connector.ts
@@ -5,9 +5,12 @@ const SPACE_001 = "Space#001";
 const USER_001 = "User#001";
 const USER_002 = "User#002";
 
+let totalLatency = 0;
+let numberOfUsers = 0;
+
 try {
   console.log(process.env.WSS_URL);
-  const ws = new WebSocket(process.env.WSS_URL0, {
+  const ws = new WebSocket(process.env.WSS_URL, {
     headers: {
       spaceId: SPACE_001,
       userId: process.argv[2] === "1" ? USER_001 : USER_002,
@@ -16,24 +19,23 @@ try {
 
   ws.on("open", (e) => {
     console.log("Socket onopen fired", e);
-
-    // ws.send(
-    //   JSON.stringify({
-    //     action: "heartbeat",
-    //     payload: JSON.stringify({
-    //       timestamp: Date.now(),
-    //     }),
-    //   })
-    // );
-    // setTimeout(() => {
-    //   console.log("inside timeout");
-    // }, 2000);
   });
 
-  ws.on("heartbeat", (e) => {
+  ws.on("message", (e: string) => {
     console.log("Socket onmessage fired", e);
 
-    ws.close(1000);
+    const payload = JSON.parse(e);
+    if (payload?.timestamp) {
+      const latency = Date.now() - payload.timestamp;
+      console.log("Latency:", latency);
+      totalLatency += latency;
+      console.log(
+        "Average Latency:",
+        Math.ceil(totalLatency / ++numberOfUsers)
+      );
+    }
+
+    // ws.close(1000);
   });
 
   ws.on("close", (e) => {
